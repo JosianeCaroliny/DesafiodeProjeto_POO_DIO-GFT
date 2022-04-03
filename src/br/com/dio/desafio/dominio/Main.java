0c09cd5992da5f288689e8a245c0be7e718f7690
Josiane <jcarolliny@gmail.com>
2022-04-03 19:07:03 -0300

    Adição das classes Main, Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,12 @@
+package br.com.dio.desafio.dominio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Curso curso = new Curso();
+
+		Mentoria mentoria = new Mentoria();
+		
+	}
+
+}
